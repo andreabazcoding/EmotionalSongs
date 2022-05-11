95ed27e4937ffb9e2d66b7a5179267b5cec886ca
AndreaBaz <abasilico5@studenti.uninsubria.it>
2022-05-11 17:44:24 +0200

    Class CanzoneEntity Added methods: - ExtractSummary (not finished)

--- src/main/java/com/studenti/uninsubria/emotionalsongs/ServerES/Entities/CanzoneEntity.java
+++ src/main/java/com/studenti/uninsubria/emotionalsongs/ServerES/Entities/CanzoneEntity.java
@@ -269,5 +269,46 @@ public class CanzoneEntity {
         return listCanzone;
     }
 
+    /**/
+    public List<CanzoneModel> ExctractSummary(int[] arrCanzoneId) throws SQLException, IOException  {
+        StringBuilder sb = new StringBuilder();
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = null;
+        CanzoneModel model = null;
+        List<CanzoneModel> listCanzone = new ArrayList<>();
+
+        try {
+            connection = connectionFactory.getConnection();
+
+            /*La query viene esequita ad ogni iterazione per ogni canzone presente nella playlist*/
+            for (int i = 0; i < arrCanzoneId.length; i++) {
+                sb.append("SELECT \"EmozioneProvabile\".\"NomeEmozione\", COUNT(\"EmozioneProvabile\".\"NomeEmozione\"), AVG(\"Emozione\".\"IntensitaEmozione\") ");
+                sb.append("FROM \"EmotionalSongs\".\"EmozioneProvabile\" ");
+                sb.append("INNER JOIN \"EmotionalSongs\".\"Emozione\" USING(\"EmozioneProvabileID\") ");
+                sb.append("INNER JOIN \"EmotionalSongs\".\"Canzone\" USING(\"CanzoneID\") ");
+                sb.append("WHERE \"Canzone\".\"CanzoneID\" = " + arrCanzoneId[i] + " ");
+                sb.append("GROUP BY (\"EmozioneProvabile\".\"NomeEmozione\");");
+
+                PreparedStatement preparedStatement = connection.prepareStatement(sb.toString());
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while (resultSet.next()){
+                    //?
+                }
+            }
+            connection.close();
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+            throw ex;
+        }
+        finally {
+            if(!connection.isClosed())
+                connection.close();
+        }
+
+        return listCanzone;
+    }
+
 
 }
